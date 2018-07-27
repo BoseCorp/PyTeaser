1e9d907ba701de28cba4e8424a0c41927a213ac1
Elizabeth Nielsen <elizabeth_nielsen@bose.com>
2018-07-27 19:03:41 -0400

    made python 3 compatible

--- pyteaser.py
+++ pyteaser.py
@@ -69,7 +69,7 @@ def SummarizeUrl(url):
     try:
         article = grab_link(url)
     except IOError:
-        print 'IOError'
+        print('IOError')
         return None
 
     if not (article and article.cleaned_text and article.title):
@@ -96,7 +96,6 @@ def Summarize(title, text):
 
     return summaries
 
-
 def grab_link(inurl):
     #extract article information using Python Goose
     from goose import Goose
@@ -104,7 +103,7 @@ def grab_link(inurl):
         article = Goose().extract(url=inurl)
         return article
     except ValueError:
-        print 'Goose failed to extract article from url'
+        print('Goose failed to extract article from url')
         return None
     return None
 
@@ -170,7 +169,7 @@ def split_words(text):
         text = regex_sub(r'[^\w ]', '', text, flags=REGEX_UNICODE)  # strip special chars
         return [x.strip('.').lower() for x in text.split()]
     except TypeError:
-        print "Error while splitting characters"
+        print("Error while splitting characters")
         return None
 
 
@@ -206,7 +205,8 @@ def split_sentences(text):
     
     sentences = regex_split(u'(?<![A-ZА-ЯЁ])([.!?]"?)(?=\s+\"?[A-ZА-ЯЁ])', text, flags=REGEX_UNICODE)
     s_iter = zip(*[iter(sentences[:-1])] * 2)
-    s_iter = [''.join(map(unicode,y)).lstrip() for y in s_iter]
+    s_iter = [''.join(map(str, y)).lstrip() for y in s_iter]
+    #s_iter = [''.join(map(unicode, y)).lstrip() for y in s_iter]
     s_iter.append(sentences[-1])
     return s_iter
 
